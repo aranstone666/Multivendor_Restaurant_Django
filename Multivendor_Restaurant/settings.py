2838bcea67f8f74f5ed13b69b3c23b2cbec8a484
aranstone666 <aranstone74@gmail.com>
2023-02-01 06:46:46 +0600

    added static files and Created the home page

--- Multivendor_Restaurant/settings.py
+++ Multivendor_Restaurant/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ["Multivendor_Restaurant/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
